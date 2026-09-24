65b7e6d4c38ae0e3b4a66047611888f8845ce682
MIOM-2025 <3349246255@QQ.com>
2026-09-25 05:35:19 +0800

    ))

--- source/funkin/backend/system/Main.hx
+++ source/funkin/backend/system/Main.hx
@@ -31,7 +31,7 @@ class Main extends Sprite
 {
 	public static var instance:Main;
 
-	public static var modToLoad:String = "VMU";
+	public static var modToLoad:String = "UNDERTALE-MIX";
 	public static var forceGPUOnlyBitmapsOff:Bool = #if (desktop || mobile) false #else true #end;
 	public static var noTerminalColor:Bool = false;
 	public static var verbose:Bool = false;
